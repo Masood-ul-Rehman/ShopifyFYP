59c65f407b67df43980c55781be541b3caf00651
Masood-ul-Rehman <masooddulrehman@gmail.com>
2023-04-01 19:39:06 +0500

    initial change

--- front_end/src/components/Header.jsx
+++ front_end/src/components/Header.jsx
@@ -17,7 +17,7 @@ function Header() {
   return (
     <header className="header">
       <div className="logo">
-        <Link to="/">GoalSetter</Link>
+        <Link to="/">Dashboard</Link>
       </div>
       <ul>
         {user ? (
