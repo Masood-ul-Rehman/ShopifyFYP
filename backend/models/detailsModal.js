4ba79d35063d870168bcc9cefd25122e943760df
waris-ansar <bhehzadprince@gmail.com>
2023-04-11 23:47:27 +0500

    first commit

---
+++ backend/models/detailsModal.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const businessSchema = mongoose.Schema(
+  {
+    User: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "user",
+    },
+    name: {
+      type: String,
+      required: [true, "Please enter Name of your Business"],
+    },
+    type: {
+      type: String,
+      required: [true, "Business type"],
+    },
+    image: {
+      data: Buffer,
+      contentType: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+module.exports = mongoose.model("detailsModal", businessSchema);
