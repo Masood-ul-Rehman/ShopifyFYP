4c7712a551d00ad2f8a339f630f94aee7345a163
Masood-ul-Rehman <81099787+Masood-ul-Rehman@users.noreply.github.com>
2023-04-12 05:35:36 +0500

    added new features

--- backend/controlers/productControler.js
+++ backend/controlers/productControler.js
@@ -2,7 +2,7 @@ const User = require("../models/userModal");
 const Product = require("../models/productModal");
 const asyncHandler = require("express-async-handler");
 const slugify = require("slugify");
-const addProduct = async (req, res) => {
+const addProduct = asyncHandler(async (req, res) => {
   try {
     const {
       User,
@@ -31,6 +31,7 @@ const addProduct = async (req, res) => {
       quantity,
       sold,
       colors,
+      image: { data: req.file.filename },
     });
     if (req.file) {
       newProduct.image = {
@@ -41,7 +42,98 @@ const addProduct = async (req, res) => {
   } catch (error) {
     throw new Error(error);
   }
-};
+});
+const updateProduct = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    if (req.body.title) {
+      req.body.slug = slugify(req.body.title);
+    }
+    const updateProduct = await Product.findOneAndUpdate(
+      { _id: id },
+      req.body,
+      {
+        returnOriginal: false,
+      }
+    );
+    res.json(updateProduct);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const deleteProduct = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deleteProduct = await Product.findOneAndDelete({ _id: id });
+    if (!deleteProduct) {
+      // Product not found
+      return res.status(404).json({ error: "Product not found" });
+    }
+    res.json(deleteProduct);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const getaProduct = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const findProduct = await Product.findById(id);
+    res.json(findProduct);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+const getAllProduct = asyncHandler(async (req, res) => {
+  try {
+    // Filtering
+    const queryObj = { ...req.query };
+    const excludeFields = ["page", "sort", "limit", "fields"];
+    excludeFields.forEach((el) => delete queryObj[el]);
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    let query = Product.find(JSON.parse(queryStr));
+
+    // Sorting
+
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    // limiting the fields
+
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
+    // pagination
+
+    const page = req.query.page;
+    const limit = req.query.limit;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+    if (req.query.page) {
+      const productCount = await Product.countDocuments();
+      if (skip >= productCount) throw new Error("This Page does not exists");
+    }
+    const product = await query;
+    res.json(product);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 module.exports = {
   addProduct,
+  updateProduct,
+  deleteProduct,
+  getaProduct,
+  getAllProduct,
 };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,20 @@
 const express = require("express");
 
-const { addProduct } = require("../controlers/productControler");
+const {
+  addProduct,
+  updateProduct,
+  deleteProduct,
+  getaProduct,
+  getAllProduct,
+} = require("../controlers/productControler");
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 const { protect } = require("../middleware/authmiddleware");
 const router = express.Router();
 
 router.post("/addProduct", protect, upload.single("image"), addProduct);
-
+router.put("/:id", protect, updateProduct);
+router.delete("/:id", deleteProduct);
+router.get("/:id", getaProduct);
+router.get("/", getAllProduct);
 module.exports = router;
