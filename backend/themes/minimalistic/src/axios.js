342f81786534e6009571c432e1ed8269c19c5f35
waris-ansar <bhehzadprince@gmail.com>
2023-05-28 15:43:19 +0500

    deleted extra files and adjusted in themes

--- backend/themes/minimalistic/src/axios.js
+++ backend/themes/minimalistic/src/axios.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-axios.defaults.baseURL = "https://bitter-hill-3387.fly.dev/api";
-// axios.defaults.baseURL = "http://localhost:8080/api";
+axios.defaults.baseURL = "http://localhost:5000/api";
 axios.interceptors.request.use(function (req) {
    const user = localStorage.getItem("user");
 
