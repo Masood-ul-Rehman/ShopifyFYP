b6b8a3d709d60dad3091a5c070c9428c15258d74
Masood-ul-Rehman <81099787+Masood-ul-Rehman@users.noreply.github.com>
2023-06-03 21:19:36 +0500

    added feature to get user stores

--- backend/controlers/userStoreControler.js
+++ backend/controlers/userStoreControler.js
@@ -1,11 +1,9 @@
 const asyncHandler = require("express-async-handler");
 const UserWebsite = require("../models/templateModal");
 const getUserStores = asyncHandler(async (req, res) => {
-  const { User } = req.body;
-  if (!User) {
-    throw new Error("User Id needed");
-  }
-  const Website = await UserWebsite.findOne({ User });
-  return Website;
+  const { id } = req.params;
+
+  const Website = await UserWebsite.findOne({ User: id });
+  res.json({ Website });
 });
 module.exports = { getUserStores };
--- backend/routes/storeRoutes.js
+++ backend/routes/storeRoutes.js
@@ -3,5 +3,5 @@ const { getUserStores } = require("../controlers/userStoreControler");
 const { protect } = require("../middleware/authmiddleware");
 const router = express.Router();
 
-router.get("/getStores", getUserStores);
+router.get("/getStores/:id", getUserStores);
 module.exports = router;
