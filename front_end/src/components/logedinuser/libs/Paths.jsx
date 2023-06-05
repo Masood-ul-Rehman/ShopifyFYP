71b341779a8bc9f271f1dfc74498f680e102f68a
waris-ansar <bhehzadprince@gmail.com>
2023-06-06 01:39:32 +0500

    fixed the routes issues

--- front_end/src/components/logedinuser/libs/Paths.jsx
+++ front_end/src/components/logedinuser/libs/Paths.jsx
@@ -1,3 +1,4 @@
+
 import {
   HiOutlineViewGrid,
   HiOutlineCube,
@@ -8,39 +9,46 @@ import {
   HiOutlineQuestionMarkCircle,
   HiOutlineCog,
 } from "react-icons/hi";
+import { useParams } from "react-router-dom";
 
-export const DASHBOARD_SIDEBAR_LINKS = [
-  {
-    key: "dashboard",
-    label: "Dashboard",
-    path: "/",
-    icon: <HiOutlineViewGrid />,
-  },
-  {
-    key: "products",
-    label: "Products",
-    path: "/products",
-    icon: <HiOutlineCube />,
-  },
-  {
-    key: "orders",
-    label: "Orders",
-    path: "/orders",
-    icon: <HiOutlineShoppingCart />,
-  },
-];
+export const getDashboardSidebarLinks = (id) => {
+  return [
+    {
+      key: "dashboard",
+      label: "Dashboard",
+      path: `/dashboard/${id}`,
+      icon: <HiOutlineViewGrid />,
+    },
+    {
+      key: "products",
+      label: "Products",
+      path: `/dashboard/${id}/products`,
+      icon: <HiOutlineCube />,
+    },
+    {
+      key: "orders",
+      label: "Orders",
+      path: `/dashboard/${id}/orders`,
+      icon: <HiOutlineShoppingCart />,
+    },
+  ];
+};
 
-export const DASHBOARD_SIDEBAR_BOTTOM_LINKS = [
-  {
-    key: "settings",
-    label: "Settings",
-    path: "/settings",
-    icon: <HiOutlineCog />,
-  },
-  {
-    key: "support",
-    label: "Help & Support",
-    path: "/support",
-    icon: <HiOutlineQuestionMarkCircle />,
-  },
-];
+export const getDashboardSidebarBottomLinks = (id) => {
+  return [
+    {
+      key: "settings",
+      label: "Settings",
+      path: `/dashboard/${id}/settings`,
+      icon: <HiOutlineCog />,
+    },
+    {
+      key: "support",
+      label: "Help & Support",
+      path: `/dashboard/${id}/support`,
+      icon: <HiOutlineQuestionMarkCircle />,
+    },
+  ];
+};
+
+
