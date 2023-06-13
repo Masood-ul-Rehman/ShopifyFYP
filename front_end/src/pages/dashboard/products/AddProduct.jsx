3fcc53dfe4aa0d99f473603f0f5813f4cfb8ed1c
Waris Ansar <76812925+waris-ansar@users.noreply.github.com>
2023-06-14 03:29:05 +0500

    added the product on first click

--- front_end/src/pages/dashboard/products/AddProduct.jsx
+++ front_end/src/pages/dashboard/products/AddProduct.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import Button from "../../../components/Button";
@@ -29,6 +29,15 @@ function AddProduct() {
     console.log(event.target.files[0] + "imgf");
   };
 
+  useEffect(() => {
+    if (selectedImage) {
+      setFormData((prevState) => ({
+        ...prevState,
+        image: selectedImage,
+      }));
+    }
+  }, [selectedImage]);
+
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
@@ -38,14 +47,14 @@ function AddProduct() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (selectedImage) {
-      setFormData((prevState) => ({
-        ...prevState,
-        image: selectedImage,
-      }));
+    // if (selectedImage) {
+    //   setFormData((prevState) => ({
+    //     ...prevState,
+    //     image: selectedImage,
+    //   }));
+    
+    // }
       dispatch(AddProductThnuk(formData));
-      
-    }
   };
 
   return (
